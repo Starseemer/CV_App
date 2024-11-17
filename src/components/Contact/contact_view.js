54a2c4b3fc84e8b4af9f5db41b72f638257372b4
Berk <berkyldzgorer@gmail.com>
2024-11-18 00:52:12 +0100

    contact form activated. minor fix.

--- src/components/Contact/contact_view.js
+++ src/components/Contact/contact_view.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './contact_view.css';
 import { SocialIcon } from 'react-social-icons'
+import emailjs from '@emailjs/browser';
 
 const ContactView = () => {
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_cvzinj8', 'template_6b2g33p', form.current, {
+        publicKey: '1QOnOPfcxT7A_wl2H',
+      })
+      .then(
+        () => {
+            alert('Thank you for your message! I will get back to you as soon as possible.');
+            form.current.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
     return (
         <div>
             <div id="title-center">
@@ -14,18 +34,18 @@ const ContactView = () => {
             <p>Email: berkyldzgorer@gmail.com</p>
             <p>Phone: +49 176 79808498</p>
             </div>
-            <form className="contact-form">
+            <form ref={form} onSubmit={sendEmail} className="contact-form">
                 <div className="form-group">
                     <label htmlFor="fullName">Full Name</label>
-                    <input type="text" id="fullName" name="fullName" className="form-control" required />
+                    <input type="text" id="fullName" name="from_name" className="form-control" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="company">Company</label>
-                    <input type="text" id="company" name="company" className="form-control" />
+                    <input type="text" id="company" name="from_company" className="form-control" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" className="form-control" required />
+                    <input type="email" id="email" name="contact_email" className="form-control" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="message">Message</label>
